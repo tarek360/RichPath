86345d2b023f15c90faf375a4b45fb78a9d44ce7
Injin Bae <eyegochild@gmail.com>
2020-06-27 00:26:04 +0900

    convert java to kotlin related on PathParser classes

---
+++ richpath/src/main/kotlin/richpath/pathparser/PathParser.kt
@@ -0,0 +1,20 @@
+package richpath.pathparser
+
+import android.graphics.Path
+import android.os.Build
+
+object PathParser {
+
+    /**
+     * @param pathData The string representing a path, the same as "d" string in svg file.
+     * @return the generated Path object.
+     */
+    fun createPathFromPathData(pathData: String?): Path {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            PathParserCompatApi21.createPathFromPathData(pathData) ?: Path()
+        } else {
+            PathParserCompat.createPathFromPathData(pathData)
+        }
+    }
+
+}
